c8533465aac01373af1d754582a13101ba4dd82e
Jon Eikholm <jone@kea.dk>
2021-10-13 10:28:11 +0200

    first

---
+++ src/test/java/jon/hiweb/HiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package jon.hiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
